9914c8d499eda243d6d9441b6a47a3baef1185c3
莊君威 <chunwei@eeit>
2017-10-06 15:24:03 +0800

    update

--- CHEWME/src/main/java/com/iii/eeit9703/crawler/AttrVO.java
+++ CHEWME/src/main/java/com/iii/eeit9703/crawler/AttrVO.java
@@ -1,5 +1,63 @@
 package com.iii.eeit9703.crawler;
 
-public class AttrVO {
+import java.sql.Blob;
 
+public class AttrVO implements java.io.Serializable{
+
+	private Integer attractionID;
+	private String name;
+	private String county;
+	private String type;
+	private String address;
+	private String tel;
+	private String intro;
+	private java.sql.Blob image; 
+	public Integer getAttractionID() {
+		return attractionID;
+	}
+	public void setAttractionID(Integer attractionID) {
+		this.attractionID = attractionID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCounty() {
+		return county;
+	}
+	public void setCounty(String county) {
+		this.county = county;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getIntro() {
+		return intro;
+	}
+	public void setIntro(String intro) {
+		this.intro = intro;
+	}
+	public Blob getImage() {
+		return image;
+	}
+	public void setImage(Blob image) {
+		this.image = image;
+	}
 }
