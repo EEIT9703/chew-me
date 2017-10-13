0b51e532b1fa9170aa10ee4b5b24d20202c20f32
廖維浪 <william82628@outlook.com>
2017-10-13 16:41:28 +0800

    更新CreatActivity.sql

--- CHEWME/src/main/java/com/iii/eeit9703/activity/CreatActivity.sql
+++ CHEWME/src/main/java/com/iii/eeit9703/activity/CreatActivity.sql
@@ -14,7 +14,7 @@ County            varchar(max),
 type              varchar(max),
 
 constraint activity_primary_key primary key (actID));
-
+ 
 --新增行程
 INSERT INTO activity VALUES ('1','09:00','無','九份老街','新北市','美食');
 INSERT INTO activity VALUES ('2','10:00','無','Hotel','新北市','住宿');
